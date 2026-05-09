3d89c635858e1299df09e039c2e92958ea4c565e
pihu098 <priyansang0@gmail.com>
2026-05-09 15:41:29 +0530

    Update jumble_game.js

--- static/jumble_game.js
+++ static/jumble_game.js
@@ -44,3 +44,101 @@ function quitGame() {
         "<button onclick='location.reload()'>Next Level</button>";
     });
 }
+
+function playWinSound(){
+
+    let sound = document.getElementById("winSound");
+
+    sound.pause();
+
+    sound.currentTime = 0;
+
+    sound.play();
+
+}
+
+function playLoseSound(){
+
+    let sound = document.getElementById("loseSound");
+
+    sound.pause();
+
+    sound.currentTime = 0;
+
+    sound.play();
+
+}
+
+
+function stopSounds(){
+
+    document.getElementById("winSound").pause();
+
+    document.getElementById("loseSound").pause();
+
+}
+
+
+function checkAnswer(selected){
+
+    fetch("/check_answer",{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify({
+            answer:selected
+        })
+
+    })
+
+    .then(res => res.json())
+
+    .then(data => {
+
+        if(data.correct){
+
+            playWinSound();
+
+            setTimeout(()=>{
+
+                alert("🎉 YOU WON");
+
+                stopSounds();
+
+                location.reload();
+
+            },1000);
+
+        }
+
+        else if(data.gameover){
+
+            playLoseSound();
+
+            setTimeout(()=>{
+
+                alert("💀 Better Luck Next Time");
+
+                stopSounds();
+
+                location.reload();
+
+            },1000);
+
+        }
+
+        else{
+
+            playLoseSound();
+
+            alert("❌ Wrong Answer");
+
+        }
+
+    });
+
+}
